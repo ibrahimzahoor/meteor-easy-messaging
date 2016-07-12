6ab303f479678692cd46f7c9eec1034bd41386e9
Ibrahim Zahoor <ibrahimzahoor.nu@gmail.com>
2016-07-13 01:05:15 +0500

    update to version 0.3.0

--- client.js
+++ client.js
@@ -8,19 +8,20 @@ import './lib/api/messages/methods.js'
 
 import './lib/api/users/user-extention.js'
 
-import './lib/ui/pages/conversations/show-page.js'
-import './lib/ui/pages/conversations/list-page.js'
-
 //components
 import './lib/ui/components/conversations/list.js'
 import './lib/ui/components/conversations/list-item.js'
-import './lib/ui/components/conversations/message-history.js'
+import './lib/ui/components/conversations/messages.js'
 import './lib/ui/components/conversations/message.js'
 import './lib/ui/components/conversations/new.js'
 import './lib/ui/components/conversations/reply.js'
 import './lib/ui/components/conversations/show.js'
 
-import './lib/ui/components/general/notification-badge.js'
+import './lib/ui/components/application/notification-badge.js'
+
+import './lib/ui/pages/conversation-list-page.js'
+import './lib/ui/pages/conversation-show-page.js'
+
 
 import './lib/ui/lib/helpers.js'
 
--- package.js
+++ package.js
@@ -1,28 +1,28 @@
 Package.describe({
   name: 'ibrahimzahoor:easy-messaging',
-  version: '0.2.0',
-  summary: 'Built on socialize packages, provide full messaging kit',
+  version: '0.3.0',
+  summary: 'Built on socialize:messaging, provide full messaging kit',
   git: 'https://github.com/ibrahimzahoor/easy-messaging.git',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.3.3');
-  api.use(['ecmascript']);
-  api.use(['templating'], 'client');
+  api.use(['ecmascript', 'check', 'underscore', 'templating']);
   api.use([
     'socialize:messaging@0.5.1',
-    'socialize:user-model@0.1.7',
     'mizzao:user-status@0.6.6',
-    'aldeed:collection2@2.9.1',
-    'aldeed:simple-schema@1.5.3',
+    'reywood:publish-composite@1.4.2',
     'tmeasday:publish-counts@0.7.3',
-    // 'socialize:user-presence@0.4.0'
   ]);
+
   api.mainModule('server.js', 'server');
   api.mainModule('client.js', 'client');
-  // api.export('Conversation');
-  // api.export('User');
+
+  api.export('Conversation');
+  api.export('Participant');
+  api.export('User');
+  api.export('Message');
 
 });
 
